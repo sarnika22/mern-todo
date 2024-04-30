548b279d030e525881ab9914cd593a1c9c880005
Sarnika K <sharu022kemp@gmail.com>
2024-04-30 22:53:08 +0530

    get put

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ app.use(cors());
 app.use(express.json()); //when data passed i'll bw converted to json format
 
 mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+app.get('/get', (req, res) => {
+  TodoModel.find()
+  .then(result => res.json(result))
+  .catch(err => res.json(err))
+})
+
 app.post("/add", (req, res) => {
   const task = req.body.task;
   console.log(task);
--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,30 +1,57 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
-import Create from './Create'
+import React, { useEffect, useState } from "react";
+import Create from "./Create";
+import axios from "axios";
+import {
+  BsCircleFill,
+  BsFillCheckCircleFill,
+  BsFillTrashFill,
+} from "react-icons/bs";
 
 function Home() {
-    const [todos, setTodos] = useState([]) //empty array to store the todo list
+  const [todos, setTodos] = useState([]); //empty array to store the todo list
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/get")
+      .then((result) => setTodos(result.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleEdit = (id) => {
+    axios
+      .put("http://localhost:3001/get")
+      .then((result) => setTodos(result.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
-        <h2>Todo List</h2>
-        <Create />
-         {
-          todos.length === 0 
-          ? //if todo is empty
-          <div>
-            <h2>No Record</h2>
-          </div>
-          : //else display todo record
-          todos.map(todo => (
-            <div>
-              {todo}
+      <h2>Todo List</h2>
+      <Create />
+      {todos.length === 0 ? (
+        //if todo is empty
+        <div>
+          <h2>No Record</h2>
+        </div>
+      ) : (
+        //else display todo record
+        todos.map((todo) => (
+          <div className="task">
+            <div className="checkbox" onClick={() => handleEdit(todo._id)}>
+              <BsCircleFill className="icon" />
+              <p>{todo.task}</p>
             </div>
-
-          ) )
-         }
+            <div>
+              <span>
+                <BsFillTrashFill className="icon" />
+              </span>
+            </div>
+          </div>
+        ))
+      )}
     </div>
-  ) 
+  );
 }
 
-export default Home
+export default Home;
